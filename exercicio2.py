d366a317325adbacf8c240861de4143b4b981b59
arturokumura <arturokumura2008@gmail.com>
2026-03-26 19:40:12 -0300

    soma de dois valores

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Informe a nota 1: "))
+nota2 = float(input("Informe a nota 2: "))
+
+media = (nota1 + nota2) / 2
+
+print("A nota do aluno é: " , media)
---
+++ soma_variaveis.py
@@ -0,0 +1,5 @@
+valor1 = float(input())
+valor2 = float(input())
+valor3 = valor1 + valor2
+
+print(valor3)
