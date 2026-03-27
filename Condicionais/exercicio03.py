db0069ef02adce0eb69d97a4e51cbc7f3e167a93
arturokumura <arturokumura2008@gmail.com>
2026-03-27 18:42:03 -0300

    Media de duas notas

---
+++ Condicionais/exercicio03.py
@@ -0,0 +1,7 @@
+nome = (input("Informe o nome do aluno: "))
+nota1 = float (input("Informe a nota 1: "))
+nota2 = float (input("Informe a nota 2: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"A media das notas de {nome} é : {media}")
