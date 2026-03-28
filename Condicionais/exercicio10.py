070c2bc84ed4ef9721966700e2b991b0eb4bdd5c
arturokumura <arturokumura2008@gmail.com>
2026-03-28 18:22:26 -0300

    maior numero

---
+++ Condicionais/exercicio10.py
@@ -0,0 +1,8 @@
+num1 = float (input("Informe o valor 1: "))
+num2 = float (input("Informe o valor 2: "))
+
+if (num1 > num2):
+    print(f"O maior número é {num1}")
+else:
+    print(f"O maior número é {num2}")
+    
