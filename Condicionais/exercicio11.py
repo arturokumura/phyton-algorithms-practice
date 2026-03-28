2cbe955a9271dcee1eeaf63205e5c04e7dff3c1f
arturokumura <arturokumura2008@gmail.com>
2026-03-28 18:32:02 -0300

    jogo fliper

---
+++ Condicionais/exercicio11.py
@@ -0,0 +1,10 @@
+P = int(input("Digite a posição de P (0 ou 1): "))
+R = int(input("Digite a posição de R (0 ou 1): "))
+
+if P == 1:
+    print("C")
+else:
+    if R == 0:
+        print("A")
+    else:
+        print("B")
