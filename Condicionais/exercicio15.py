c71d9940a0332384e1de7675b746f011cb45646a
arturokumura <arturokumura2008@gmail.com>
2026-03-29 14:11:06 -0300

    Maior valor de 3 numeros

---
+++ Condicionais/exercicio15.py
@@ -0,0 +1,10 @@
+num1 = float(input("Informe o numero 1: "))
+num2 = float(input("Informe o numero 2: "))
+num3 = float(input("Informe o numero 3: "))
+
+if(num1 > num2):
+    print(f"O maior número é {num1}")
+elif(num2 > num3):
+    print(f"O maior númeor é {num2}")
+else:
+    print(f"O maior número é {num3}")
