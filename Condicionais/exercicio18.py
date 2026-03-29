61b9fca1dff869149c3df5178f136f85194e7833
arturokumura <arturokumura2008@gmail.com>
2026-03-29 18:09:11 -0300

    Exercicios da OBI

--- Condicionais/exercicio18.py
+++ Condicionais/exercicio18.py
@@ -1,3 +1,10 @@
+#● Uma determinada loja está fazendo promoções de vendas. Qualquer compra que
+#um cliente fizer até R$ 100,00 receberá 5% de desconto. Se a compra for maior
+#que R$ 100,00, mas inferior a R$ 200,00, o desconto será de 10%. Se for superior
+#ou igual a R$ 200,00, o desconto será de 20%.
+#● Faça um programa que calcule o desconto do total da compra de um cliente, e
+#informe também, o total a pagar já com os descontos
+
 valor = float(input("Digite o valor da compra: R$ "))
 
 if valor <= 100:
