cc4338085d846131bebba580d620cd2475e7b2e6
arturokumura <arturokumura2008@gmail.com>
2026-04-02 08:14:28 -0300

    Base e expoente

---
+++ Estruturas de Repetição/exercicio18.py
@@ -0,0 +1,10 @@
+#Dados números inteiros base e expoente, com expoente >= 0, calcular base elevado
+#a expoente sem utilizar o operador de potência. Por exemplo, dados os números 3 e
+#4 o seu programa deve escrever o número 81.
+
+base = int(input("Base: "))
+expoente = int(input("Expoente: "))
+
+resultado = base ** expoente
+
+print(resultado)
