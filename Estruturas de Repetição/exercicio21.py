fb2601f574eba47806af3cf8222230f6cc28c6a9
arturokumura <arturokumura2008@gmail.com>
2026-04-02 09:24:36 -0300

    .

--- Estruturas de Repetição/exercicio21.py
+++ Estruturas de Repetição/exercicio21.py
@@ -1,3 +1,6 @@
+#Dado um número inteiro de qualquer tamanho (inteiro longo), fornecido pelo
+#usuário via teclado, elabore um programa para determinar a soma dos dígitos que
+#estiverem em posições ímpares (da esquerda para a direita
 n = int(input("Informe um número: "))
 soma = 0
 while(n != 0):
