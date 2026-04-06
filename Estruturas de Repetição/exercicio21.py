bfa366f5c69da559a8ac3a44164af211cd5948f0
arturokumura <arturokumura2008@gmail.com>
2026-04-06 18:13:53 -0300

    Soma dos números na posição impar

--- Estruturas de Repetição/exercicio21.py
+++ Estruturas de Repetição/exercicio21.py
@@ -1,10 +1,26 @@
 #Dado um número inteiro de qualquer tamanho (inteiro longo), fornecido pelo
 #usuário via teclado, elabore um programa para determinar a soma dos dígitos que
 #estiverem em posições ímpares (da esquerda para a direita
-n = int(input("Informe um número: "))
+num = int(input("Digite um numero: "))
+
+# 1. Descobrir quantidade de dígitos
+total_digitos = 0
+temp = num
+while temp > 0:
+    temp //= 10
+    total_digitos += 1
+
+# 2. Processar da direita pra esquerda
+contador = total_digitos
 soma = 0
-while(n != 0):
-    d = n % 10
-    soma += d - (n % 10)
-n = n//10
+
+while num > 0:
+    digito = num % 10
+    
+    if contador % 2 != 0:  # posição ímpar (da esquerda)
+        soma += digito
+    
+    num //= 10
+    contador -= 1
+
 print(soma)
