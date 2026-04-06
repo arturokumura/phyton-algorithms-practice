93bacaa82ac497698c8ed24066ee90ecd097e057
arturokumura <arturokumura2008@gmail.com>
2026-04-06 09:09:52 -0300

    Modificação de Strings

---
+++ Strings/modificação_strings.py
@@ -0,0 +1,12 @@
+frase1 = "Boa noite com melodia"
+frase2  = frase1[:14] + "alegria!"
+print(frase2)
+
+print("---------------------------------")
+
+frase1 = "Boa noite com melodia"
+frase2 = ""
+for i in range(14):
+    frase2 = frase2 + frase1[i]
+frase2 += "alegria!"
+print(frase2)
