68d454fdf95b613b8a9f84ee4226ab95934f6a46
Artur Okumura <arturokumura2008@gmail.com>
2026-04-06 11:37:33 -0300

    Encontrar indice de uma letra em uma frase

--- Strings/ex04.py
+++ Strings/ex04.py
@@ -1,4 +1,4 @@
-'''● Faça um programa que leia uma frase e mostre a posição da última ocorrência de
+''''● Faça um programa que leia uma frase e mostre a posição da última ocorrência de
 uma letra informada pelo usuário na frase.
 ● Entradas: frase e letra a ser encontrada (última ocorrência da esquerda para a direita
 '''
