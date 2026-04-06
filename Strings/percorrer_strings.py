f50d2fc755c6a15da4912821a55a7971e731e7b3
arturokumura <arturokumura2008@gmail.com>
2026-04-06 08:54:13 -0300

    Formas de percorrer Strings

---
+++ Strings/percorrer_strings.py
@@ -0,0 +1,18 @@
+palavra = "IFSP"
+
+print("--------------------------")
+
+i = 0
+while i < len(palavra):
+    print(palavra[i])
+    i = i + 1
+    
+print("--------------------------")
+
+for i in range(len(palavra)):
+    print(palavra[i])
+    
+print("--------------------------")
+
+for c in palavra:
+    print(c)
