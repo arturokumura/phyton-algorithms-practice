a764171874df18072406469dba83aa9b18d7e754
arturokumura <arturokumura2008@gmail.com>
2026-04-08 17:42:56 -0300

    Cálculo rápido

---
+++ OBI/exercicio07.py
@@ -0,0 +1,31 @@
+#Algumas pessoas conseguem fazer cálculos matemáticos com uma velocidade
+#impressionante. Laurinha tem essa habilidade! Um cálculo que ela consegue fazer muito
+#rapidamente é: dados três números inteiros S, A, e B, determinar quantos números do
+#intervalo [A, B] têm a soma de seus dígitos igual a S.
+#Por exemplo, se S = 3, A = 10 e B = 30, então a reposta é 3, pois existem três números no
+#intervalo [10,30] cuja soma dos dígitos é igual a três: 12, 21 e 30.
+#Sua tarefa é escrever um programa de computador para, dados os três números, tentar
+#calcular a resposta mais rapidamente do que Laurinha consegue.
+#Entrada
+#A primeira linha da entrada contém um número inteiro S, o valor da soma dos dígitos.
+#A segunda e a terceira linhas contêm respectivamente os inteiros A e B.
+#Saída
+#Seu programa deve produzir uma única linha, contendo um único inteiro, quantos
+#números no intervalo dado têm a soma de dígitos indicada.
+
+
+S = int(input("Soma: "))
+A = int(input("Início do intervalo: "))
+B = int(input("Fim do intervalo: "))
+
+contador = 0
+
+for num in range(A, B + 1):
+    soma_digitos = 0
+    while num > 0:
+      digito = num % 10
+      soma_digitos += digito
+      num = num // 10
+    if soma_digitos == S:
+        contador += 1
+print(contador)
