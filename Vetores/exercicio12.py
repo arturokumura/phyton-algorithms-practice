74f6e313bf9a8c6de00490e8372e3de0d8983cd7
arturokumura <arturokumura2008@gmail.com>
2026-05-07 15:03:58 -0300

    Remover primeira ocorrencia

--- Vetores/exercicio12.py
+++ Vetores/exercicio12.py
@@ -3,10 +3,25 @@ Em seguida leia um número inteiro X e remova a primeira ocorrência desse núme
 da lista. Mostre a lista resultante.
 ○ A respeito de funções de lista, só é permitido o uso da função append(), len() e del()'''
 N = int(input("Quantos números ler? "))
+x = int(input("Informe um número para remover: "))
 lista = []
-achou = False
+resultado = []
+pos = 0
+parar = False
+cont = 0
 for i in range(N):
-    x = int(input("Leia um número: "))
-    lista.append(x)
+    n = int(input("Leia um número: "))
+    lista.append(n)
+
+for j in range(len(lista)):
+    if lista[j] == x and not parar:
+        pos = j
+        resultado = lista[:pos] + lista[pos + 1:]
+        parar = True
+    elif lista[j] != x:
+        cont += 1
+        if cont == len(lista):
+            print("Número não presente na lista")
+print(resultado)
 
 
