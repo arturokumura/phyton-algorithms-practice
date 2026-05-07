bfd2192a04fe461c817a703a829b83d8eb3ddb2f
arturokumura <arturokumura2008@gmail.com>
2026-05-07 19:22:33 -0300

    Soma dos elementos de uma matriz

--- Matrizes/Instanciar_matriz.py
+++ Matrizes/Instanciar_matriz.py
@@ -1,17 +1,34 @@
-# matriz 3x4
-linhas = 3
-colunas = 4
+'''1. Faça um programa que leia 9 valores um a um e coloque em uma matriz 3x3. Após
+isso, faça:
+○ Imprima os 9 elementos no formato matricial
+○ Imprima a soma total dos elementos
+○ Imprima a soma dos elementos de cada linha'''
 
-# inicializa a matriz
 mat = []
+linhas = 3
+colunas = 3
 
-for i in range(linhas):  # linhas
+for i in range(linhas):
     linha = []
-
-    for j in range(colunas):  # colunas
-        x = int(input(f"Informe o numero [{i}][{j}]: "))
+    for j in range(colunas):
+        x = int(input(f"Insira o valor de [{i}][{j}]: "))
         linha.append(x)
-
     mat.append(linha)
 
-print(mat)
+#Imprimir no formato matricial
+for linha in mat:
+    print(linha)
+
+#Soma total dos elementos
+soma = 0
+for linha in mat:
+    for elemento in linha:
+        soma += elemento
+print(f"Soma de todos os elementos: {soma}")
+
+# Soma de cada linha
+for linha in mat:
+    soma_linha = 0
+    for i in range(len(linha)):
+        soma_linha += linha[i]
+    print(f"Soma da linha {linha}: {soma_linha}")
