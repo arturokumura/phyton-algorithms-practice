fb01bd171316fe704865a9461793e2e259a91f8f
arturokumura <arturokumura2008@gmail.com>
2026-05-11 21:00:07 -0300

    Função escreve_linha com parametro

---
+++ Funções/exercicio02.py
@@ -0,0 +1,11 @@
+def escreve_linha(n):
+    s = ""
+    for i in range(n):
+        s += "-"
+    return s
+
+res = escreve_linha(3)
+print(res)
+print("Instituto Federal")
+res = escreve_linha(3)
+print(res)
